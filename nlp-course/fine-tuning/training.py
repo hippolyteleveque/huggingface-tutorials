44b46ba3787662e93a163865bdee4064bc30158e
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-04-15 20:03:49 +0200

    Added the training code for Hugging Face.

---
+++ nlp-course/fine-tuning/training.py
@@ -0,0 +1,46 @@
+import numpy as np
+import evaluate
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding, TrainingArguments, AutoModelForSequenceClassification, Trainer
+
+raw_datasets = load_dataset("glue", "mrpc")
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+training_args = TrainingArguments("test-trainer")
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    checkpoint, num_labels=2)
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer
+)
+
+# trainer.train()
+
+predictions = trainer.predict(tokenized_datasets["validation"])
+
+
+def compute_metrics(eval_preds):
+    metric = evaluate.load("glue", "mrpc")
+    logits, labels = eval_preds
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
+if __name__ == "__main__":
+    pass
